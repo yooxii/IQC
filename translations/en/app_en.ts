611e45268e05918fdf4a9a9fb45af7def95f0265
LucasLi <22400616@acbel.com>
2025-08-19 16:58:26 +0800

    docs: 添加英文和中文翻译文件 介绍: 添加介绍的ppt

---
+++ translations/en/app_en.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="48"/>
+        <source>串口连接成功</source>
+        <translation>Serial port connection successful</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="329"/>
+        <location filename="../../src/ui/__init__.py" line="331"/>
+        <source>Light</source>
+        <translation>Light</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="333"/>
+        <source>Dark</source>
+        <translation>Dark</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="421"/>
+        <source>status:</source>
+        <translation>status:</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="439"/>
+        <source>Warning of Misoperation</source>
+        <translation>Warning of Misoperation</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="440"/>
+        <source>Should the serial port connection be disconnected?</source>
+        <translation>Should the serial port connection be disconnected?</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="465"/>
+        <source>Data acquisition timeout!</source>
+        <translation>Data acquisition timeout!</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="478"/>
+        <location filename="../../src/ui/__init__.py" line="494"/>
+        <source>数据获取完成</source>
+        <translation>Data acquisition completed</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="498"/>
+        <source>数据获取重试次数:</source>
+        <translation>Data acquisition retry count:</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="591"/>
+        <source>Not the measurement interface!</source>
+        <translation>Not the measurement interface!</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="605"/>
+        <source>No data can be saved!</source>
+        <translation>No data can be saved!</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="609"/>
+        <source>Save the data as</source>
+        <translation>Save the data as</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="631"/>
+        <source>Reset</source>
+        <translation>Reset</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="632"/>
+        <source>Should all data be reset?</source>
+        <translation>Should all data be reset?</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="653"/>
+        <source>Count of components</source>
+        <translation>Count of components</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="658"/>
+        <source>The test count is zero, but the component count still increases?</source>
+        <translation>The test count is zero, but the component count still increases?</translation>
+    </message>
+</context>
+<context>
+    <name>setsDialog</name>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="95"/>
+        <source>Light</source>
+        <translation>Light</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/zh/app_zh.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="48"/>
+        <source>串口连接成功</source>
+        <translation>串口连接成功</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="329"/>
+        <location filename="../../src/ui/__init__.py" line="331"/>
+        <source>Light</source>
+        <translation>浅色</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="333"/>
+        <source>Dark</source>
+        <translation>深色</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="421"/>
+        <source>status:</source>
+        <translation>状态:</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="439"/>
+        <source>Warning of Misoperation</source>
+        <translation>误操作警告</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="440"/>
+        <source>Should the serial port connection be disconnected?</source>
+        <translation>是否断开串口连接？</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="465"/>
+        <source>Data acquisition timeout!</source>
+        <translation>数据获取超时！</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="478"/>
+        <location filename="../../src/ui/__init__.py" line="494"/>
+        <source>数据获取完成</source>
+        <translation>数据获取完成</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="498"/>
+        <source>数据获取重试次数:</source>
+        <translation>数据获取重试次数:</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="591"/>
+        <source>Not the measurement interface!</source>
+        <translation>不是测量页面！</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="605"/>
+        <source>No data can be saved!</source>
+        <translation>没有数据可以保存！</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="609"/>
+        <source>Save the data as</source>
+        <translation>将数据保存为</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="631"/>
+        <source>Reset</source>
+        <translation>重置</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="632"/>
+        <source>Should all data be reset?</source>
+        <translation>重置所有数据吗？</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="653"/>
+        <source>Count of components</source>
+        <translation>零件计数</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="658"/>
+        <source>The test count is zero, but the component count still increases?</source>
+        <translation>测试次数为零，仍然增加零件计数吗？</translation>
+    </message>
+</context>
+<context>
+    <name>setsDialog</name>
+    <message>
+        <location filename="../../src/ui/__init__.py" line="95"/>
+        <source>Light</source>
+        <translation>浅色</translation>
+    </message>
+</context>
+</TS>
